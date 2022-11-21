66d19f4bf98697fc7f91ec5cdf00a75a9517483f
Samir Nagib <samir.nagib@gmail.com>
2022-11-21 20:04:34 -0300

    05-12 URI 2621 Spring Boot SQL e JPQL

---
+++ uri2621/src/main/java/com/devsuperior/uri2621/projections/ProductMinProjetion.java
@@ -0,0 +1,6 @@
+package com.devsuperior.uri2621.projections;
+
+public interface ProductMinProjetion {
+
+	String getName();
+}
--- uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,9 +1,27 @@
 package com.devsuperior.uri2621.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.uri2621.dto.ProductMinDTO;
 import com.devsuperior.uri2621.entities.Product;
+import com.devsuperior.uri2621.projections.ProductMinProjetion;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	
+	@Query(nativeQuery = true, value = "select products.name "
+	 		+ "from products "
+	 		+ "inner join providers ON providers.id = products.id_providers "
+	 		+ "where products.amount between :min and :max and "
+	 		+ "providers.name like CONCAT(:beginName,'%')") 
+	List<ProductMinProjetion> search1(Integer min, Integer max, String beginName);
+	
+	@Query("select new com.devsuperior.uri2621.dto.ProductMinDTO(obj.name) "
+	 		+ "from Product obj "
+	 		+ "where obj.amount between :min and :max and "
+	 		+ "obj.provider.name like CONCAT(:beginName,'%')") 
+	List<ProductMinDTO> search2(Integer min, Integer max, String beginName);
+	
 }
